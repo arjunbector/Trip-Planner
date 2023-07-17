6e3d500835182ba7df3bf3d0f9cce896b9ad727d
arjunbector <arjunbector@gmail.com>
2023-07-17 21:24:24 +0530

    a

--- javascript/items.js
+++ javascript/items.js
@@ -8,6 +8,7 @@ if (page == "index.html") {
     <li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
     <li class="item"><a class="navbar_btn " href="./index.html#page-footer">Contact</a></li>
     </ul>`;
+    console.log("TRUEEEE")
 }
  navbar = `<ul>
     <li class="item"><a class="navbar_btn active" href="../index.html">Home</a></li>
