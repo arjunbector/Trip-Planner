d65d106a80e39912a3d4235c37107542ebbfacb1
arjunbector <arjunbector@gmail.com>
2023-07-17 20:58:18 +0530

    Edited path of index.html

--- javascript/items.js
+++ javascript/items.js
@@ -2,7 +2,7 @@ const navbar = `<ul>
 <li class="item"><a class="navbar_btn active" href="./index.html">Home</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#destinations">Trips</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
-<li class="item"><a class="navbar_btn " href="./index.html#">Contact</a></li>
+<li class="item"><a class="navbar_btn " href="./index.html#page-footer">Contact</a></li>
 </ul>`
 
 document.getElementById("navbar").innerHTML = navbar;
