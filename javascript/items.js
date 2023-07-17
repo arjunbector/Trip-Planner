e939ae4327157d1ef2180ef3559cb2000b1fd412
arjunbector <arjunbector@gmail.com>
2023-07-17 21:18:23 +0530

    home btn

--- javascript/items.js
+++ javascript/items.js
@@ -1,10 +1,20 @@
-const navbar = `<ul>
-<li class="item"><a class="navbar_btn active" href="../index.html">Home</a></li>
-<li class="item"><a class="navbar_btn " href="./index.html#destinations">Trips</a></li>
-<li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
-<li class="item"><a class="navbar_btn " href="./index.html#page-footer">Contact</a></li>
-</ul>`
+var path = window.location.pathname;
+var page = path.split("/").pop();
+let navbar = "";
+if (page == "index.html") {
+  navbar = `<ul>
+    <li class="item"><a class="navbar_btn active" href="./index.html">Home</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#destinations">Trips</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#page-footer">Contact</a></li>
+    </ul>`;
+}
+ navbar = `<ul>
+    <li class="item"><a class="navbar_btn active" href="../index.html">Home</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#destinations">Trips</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#page-footer">Contact</a></li>
+    </ul>`;
 
 document.getElementById("navbar").innerHTML = navbar;
-
-const footer = ``
+const footer = ``;
