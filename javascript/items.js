84388e3059b55a3e04be3865b5a90a77c3b150a9
arjunbector <arjunbector@gmail.com>
2023-07-17 21:02:02 +0530

    Fixed the home btn bug in navbar

--- javascript/items.js
+++ javascript/items.js
@@ -1,5 +1,5 @@
 const navbar = `<ul>
-<li class="item"><a class="navbar_btn active" href="./index.html">Home</a></li>
+<li class="item"><a class="navbar_btn active" href="../index.html">Home</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#destinations">Trips</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#page-footer">Contact</a></li>
