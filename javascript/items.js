6e2c95845b5c89075c375dc57db1876d5500057f
arjunbector <arjunbector@gmail.com>
2023-07-17 12:49:17 +0530

    Added things to explore section

--- javascript/items.js
+++ javascript/items.js
@@ -1,8 +1,9 @@
-let code = `<ul>
+const navbar = `<ul>
 <li class="item"><a class="navbar_btn active" href="./index.html">Home</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#destinations">Trips</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
 <li class="item"><a class="navbar_btn " href="./index.html#">Contact</a></li>
 </ul>`
 
-document.getElementById("navbar").innerHTML = code;
+document.getElementById("navbar").innerHTML = navbar;
+
