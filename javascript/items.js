3dc2e9ba911d9c0c818e0b71681f9ce0aa41a838
arjunbector <arjunbector@gmail.com>
2023-07-19 10:32:54 +0530

    set the paths

--- javascript/items.js
+++ javascript/items.js
@@ -2,10 +2,10 @@ var path = window.location.pathname;
 var page = path.split("/").pop();
 
  const navbar = `<ul>
-    <li class="item"><a class="navbar_btn active" href="/index.html">Home</a></li>
-    <li class="item"><a class="navbar_btn " href="/index.html#destinations">Trips</a></li>
-    <li class="item"><a class="navbar_btn " href="/index.html#home_resorts">Resorts</a></li>
-    <li class="item"><a class="navbar_btn " href="/index.html#page-footer">Contact</a></li>
+    <li class="item"><a class="navbar_btn active" href="./index.html">Home</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#destinations">Trips</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#home_resorts">Resorts</a></li>
+    <li class="item"><a class="navbar_btn " href="./index.html#page-footer">Contact</a></li>
     </ul>`;
 
 document.getElementById("navbar").innerHTML = navbar;
