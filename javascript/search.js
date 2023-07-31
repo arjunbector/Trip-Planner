3cce0205375459adea583696a406715eeefb74fc
arjunbector <arjunbector@gmail.com>
2023-07-31 21:57:34 +0530

    Revert "Added search functionality"
    This reverts commit 7114a333c6f010d65226e289990d0a7fef1805f6.

--- javascript/search.js
+++ javascript/search.js
@@ -12,22 +12,22 @@ document.getElementById("search-input").addEventListener('input', ()=>{
     }
     const search_arr = [];
     locations_arr.forEach((dest)=>{
-        if (dest.includes(user_input)){
+        if (dest.startsWith(user_input)){
             search_arr.push(dest)
             console.log(search_arr);
         }
     })
     let output = "";
-    if (search_arr.length == 0){
-        output = `<div class = "search-result-div no-hover"></div> <div class = "search-result-div no-hover">No results found</div>`;
-        document.getElementById("search-results").innerHTML = output;
-    }
-    else{
-        output = `<div></div>` 
-        search_arr.forEach((dest)=>{
-            output += `<div class = "search-result-div">${dest.toUpperCase()}</div>`
-        document.getElementById("search-results").innerHTML = output;
-        })
-    }
+    // if (search_arr.length == 0){
+    //     output = `<div class = "search-result-div"></div> <div class = "search-result-div">No results found</div>`;
+    //     document.getElementById("search-results").innerHTML = output;
+    // }
+    // else{
+    //     output = `<div class = "search-result-div"></div>` 
+    //     search_arr.forEach((dest)=>{
+    //         output += `<div class = "search-result-div">${dest.toUpperCase()}</div>`
+    //     document.getElementById("search-results").innerHTML = output;
+    //     })
+    // }
     console.log(user_input);
-}
+})
